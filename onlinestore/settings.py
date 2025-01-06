f203a01aed06fdd984be4f043ee4b84974ba64ef
owoicho <michaelogaje033@gmail.com>
2025-01-06 10:02:36 +0100

    allowed host

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ministore-ulaz.onrender.com', 'localhost', 'ministore-ulaz.onrender.com']
 
 
 # Application definition
